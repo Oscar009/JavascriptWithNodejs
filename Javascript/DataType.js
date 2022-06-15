dce0bb3f7dbab726162f5f53182798006c432ee1
Oscar Noe Ortiz Barba <oortizbarba@gmail.com>
2022-06-14 22:47:42 -0500

    Update

---
+++ Javascript/DataType.js
@@ -0,0 +1,46 @@
+// Comment
+/**
+Multiline comment
+*/
+
+/**
+ * Data types
+ */
+
+// number
+console.log(27);
+
+// float
+console.log(2.01);
+
+// string
+console.log("Oscar");
+console.log('Oscar');
+console.log(`Oscar`);
+
+// boolean
+console.log(true);
+console.log(false);
+
+// undefined
+console.log(undefined);
+
+// null
+console.log(null);
+
+/**
+ * Structs
+ */
+// Object
+console.log({
+  name: "Oscar",
+  years: 21,
+  birthDate: null,
+  isTall: false,
+  cities: ["Quito", "LA", "San Francisco"],
+  address: { Street: "Delicias" },
+});
+
+// Array
+console.log(["Mexico", "Canada", "France", "Germany", "United States"]);
+console.log([21, true, { name: "Noe" }, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]]);
